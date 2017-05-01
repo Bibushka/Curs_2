31ed0b88df68934095a5384d4aa6dcdf4644e375
Bibushka <bianca_bucur03@yahoo.com>
2017-05-01 23:49:26 +0300

    FarmLand test

---
+++ FarmingLand/FarmingLand/FarmingLand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FarmingLand
+{
+    [TestClass]
+    public class FarmingLand
+    {
+        [TestMethod]
+        public void GenerateLandLength()
+        {
+            double landLength = GetInitialLandSurface(770000, 230);
+            Assert.AreEqual(770, landLength);
+        }
+
+        public double GetInitialLandSurface(int totalSurface, int secondSurfaceWidth)
+        {
+            double x = (Math.Sqrt(Math.Pow(secondSurfaceWidth,2) + 4 * totalSurface) - secondSurfaceWidth) / 2;
+            return x;
+        }
+    }
+}
