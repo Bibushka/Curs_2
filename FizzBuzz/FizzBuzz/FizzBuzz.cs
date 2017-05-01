cbe70622018a852cb28c5704e66f4b5cde701d18
Bibushka <bianca_bucur03@yahoo.com>
2017-05-01 23:59:55 +0300

    FizzBuzz test: error: "not all code paths return a value"

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class FizzBuzz
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string result = TalkWierd(15);
+            Assert.AreEqual("FizzBuzz", result);
+        }
+
+        public string TalkWierd(int number)
+        {
+            if (number % 3 == 0)
+                if (number % 5 == 0)
+                    return "FizzBuzz";
+                else
+                    return "Fizz";
+            if (number % 5 == 0)
+                return "Buzz";
+
+        }
+    }
+}
