8654eb4144b6a87cea056e8a105e09d653194ae9
Bibushka <bianca_bucur03@yahoo.com>
2017-05-02 23:02:22 +0300

    FizzBuzz test: FizzBuzz test

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -9,14 +9,14 @@ namespace FizzBuzz
         [TestMethod]
         public void TestMethod1()
         {
-            string result = TalkWierd(5);
-            Assert.AreEqual("Buzz", result);
+            string result = TalkWierd(15);
+            Assert.AreEqual("FizzBuzz", result);
         }
 
         public string TalkWierd(int number)
         {
-            if (number % 5 == 0)
-                return "Buzz";
+            if (number % 5 == 0 && number % 3 ==0)
+                return "FizzBuzz";
             return "Not";  
          }
     }
