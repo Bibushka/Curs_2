518abe0d91dc997d767078ed1f0f1c0c006fe966
Bibushka <bianca_bucur03@yahoo.com>
2017-05-03 18:13:33 +0300

    RomanNumbers: final

--- RomanNumbers/RomanNumbers/RomanNumbers.cs
+++ RomanNumbers/RomanNumbers/RomanNumbers.cs
@@ -6,13 +6,7 @@ namespace RomanNumbers
     [TestClass]
     public class RomanNumbers
     {
-        [TestMethod]
-        public void GenerateRomanNumbersUnderTen()
-        {
-            string romanNumber = ConvertNumbers(8);
-            Assert.AreEqual("Please enter a number smaller than 9.",romanNumber);
-        }
-
+       [TestMethod]
         public void GenerateRomanNumbers()
         {
             string romanNumber = ConvertNumbers(8);
@@ -20,25 +14,17 @@ namespace RomanNumbers
         }
 
         public string ConvertNumbers(int x)
-        {
+        { 
             string[] romanNumbers = { "IX", "V", "IV", "I" };
             int[] arabicNumbers = { 9, 5, 4, 1};
             string value=null;
-            if (x > 9 || x == 0)
-                return "Please enter a number smaller than 9.";
             for(int i=0; i<=3; i++)
-                while (x!=0)
-                    {
-                    if (x - arabicNumbers[i] == 0)
-                        {
-                        value = value + romanNumbers[i];
-                        }
+                while (x>=arabicNumbers[i])
                     if (x - arabicNumbers[i] >= 0)
                         {
                         x = x - arabicNumbers[i];
                         value = value + romanNumbers[i];
                         }
-                     }
             return value;
         }
     }
