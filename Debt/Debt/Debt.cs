a7e1edde10bb16415bcafa9e245683b1c5f71c82
Bibushka <bianca_bucur03@yahoo.com>
2017-05-05 23:40:40 +0300

    Debt test_red

---
+++ Debt/Debt/Debt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Debt
+{
+    [TestClass]
+    public class Debt
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double totalAmountOwed = CalculateMoneyOwed(100, 0);
+            Assert.AreEqual(100, totalAmountOwed);
+        }
+
+        public int CalculateMoneyOwed(int monthlyPayment, int numberOfDaysLate)
+        {
+            //double totalAmountOwed;
+            if (numberOfDaysLate == 0)
+                return 0;
+            return 1;
+        }
+    }
+}
