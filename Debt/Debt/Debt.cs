9a09bac5fc48e9d3455b38a784a24123194ee042
Bibushka <bianca_bucur03@yahoo.com>
2017-05-06 11:02:08 +0300

    Debt test: green_5

--- Debt/Debt/Debt.cs
+++ Debt/Debt/Debt.cs
@@ -9,8 +9,8 @@ namespace Debt
         [TestMethod]
         public void TestMethod1()
         {
-            double totalAmountOwed = CalculateMoneyOwed(100, 0);
-            Assert.AreEqual(100, totalAmountOwed);
+            double totalAmountOwed = CalculateMoneyOwed(100, 32);
+            Assert.AreEqual(520, totalAmountOwed);
         }
 
         public double CalculateMoneyOwed(int monthlyPayment, int numberOfDaysLate)
@@ -18,7 +18,12 @@ namespace Debt
             double totalAmountOwed=0;
             if (numberOfDaysLate == 0)
                 return monthlyPayment;
-            
+            if (numberOfDaysLate >= 1 && numberOfDaysLate <= 10)
+                totalAmountOwed = monthlyPayment + numberOfDaysLate * monthlyPayment * 0.02;
+            if (numberOfDaysLate >= 11 && numberOfDaysLate <= 30)
+                totalAmountOwed = monthlyPayment + numberOfDaysLate * monthlyPayment * 0.05;
+            if (numberOfDaysLate >= 31 && numberOfDaysLate <= 40)
+                totalAmountOwed = 2*monthlyPayment + numberOfDaysLate * monthlyPayment * 0.10;
             return totalAmountOwed;
          }
     }
