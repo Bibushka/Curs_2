fd8abcd02b96cf2927828417910c4ab4ed95c007
Bibushka <bianca_bucur03@yahoo.com>
2017-05-06 15:00:56 +0300

    Taxi test: red_ error "not all code paths return a value"
    Imi da eroarea asta, chiar daca am un return 0; in afara if-urilor

--- Taxi/Taxi/Taxi.cs
+++ Taxi/Taxi/Taxi.cs
@@ -7,14 +7,26 @@ namespace Taxi
     public class Taxi
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetDayPrice()
         {
             int fare = CalculateCabFare(8, 5);
-            Assert.AreEqual(6, fare);
+            Assert.AreEqual(25, fare);
+        }
+
+        [TestMethod]
+        public int GetNightPrice()
+        {
+            int fare = CalculateCabFare(22, 16);
+            Assert.AreEqual(112, fare);
         }
 
         public int CalculateCabFare(int hour, int distance)
         {
+            int fare;
+            if (hour >= 8 && hour < 21)
+                return fare = distance * 5;
+            if (hour >= 21 && hour < 8)
+                return fare = distance * 7;
             return 0;
         }
     }
