5c5fe4cec3c90e084f266d727fe94cd177ec3271
Bibushka <bianca_bucur03@yahoo.com>
2017-05-06 14:38:31 +0300

    Taxi test: red

---
+++ Taxi/Taxi/Taxi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Taxi
+{
+    [TestClass]
+    public class Taxi
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int fare = CalculateCabFare(8, 5);
+            Assert.AreEqual(6, fare);
+        }
+
+        public int CalculateCabFare(int hour, int distance)
+        {
+            return 0;
+        }
+    }
+}
