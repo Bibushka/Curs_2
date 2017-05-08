462ffdf36659bc8c31c6102d76dac51e9905d576
Bibushka <bianca_bucur03@yahoo.com>
2017-05-08 20:48:49 +0300

    Taxi test: green_3

--- Taxi/Taxi/Taxi.cs
+++ Taxi/Taxi/Taxi.cs
@@ -10,7 +10,14 @@ namespace Taxi
         public void TestDayChargeDistanceOne()
         {
             int fare = CalculateTaxiFare(8, 15);
-            Assert.AreEqual(75, fare);
+            Assert.AreEqual(105, fare);
+        }
+
+        [TestMethod]
+        public void TestDayChargeDistanceTwo()
+        {
+            int fare = CalculateTaxiFare(8, 50);
+            Assert.AreEqual(250, fare);
         }
 
         [TestMethod]
@@ -24,9 +31,13 @@ namespace Taxi
         public int CalculateTaxiFare(int hour, int distance)
         {
             int fare = 0;
-            if(hour>=8 && hour<21)
-                if(distance>=1 && distance<20)
-                    fare=distance*5;
+            if (hour >= 8 && hour < 21)
+            {
+                if (distance >= 1 && distance < 20)
+                    fare = distance * 7;
+                if (distance >= 20 && distance < 60)
+                    fare = distance * 5;
+            }
             if (hour < 8 || hour >= 21 && hour<=24)
                 fare = distance * 7;
             return fare;
